95670150230f6db11ce4bcec4c15e9f10c2c8ca1
Jason Laning <jason.laning@gmail.com>
2017-07-01 23:13:35 -0400

    img alt text

--- src/components/dashboard/user-profile.js
+++ src/components/dashboard/user-profile.js
@@ -5,7 +5,7 @@ import './user-profile.css';
 const UserProfile = (props) => (
 	<section className="user-profile">
 		<div className="wrapper">
-			<img src="/blank-profile-pic.png" />
+			<img src="/blank-profile-pic.png" alt="profile image" />
 	        <h1>Username</h1>
 	        <p>[User location]</p>
 	        <p>[About user] My aesthetic interests center around post-conceptual blah blah blah.</p>
