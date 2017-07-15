00c391308808dd5a2766908d0b2580f405432e10
Jason Laning <jason.laning@gmail.com>
2017-07-14 20:25:49 -0400

    image url

--- src/components/dashboard/discussions-item.js
+++ src/components/dashboard/discussions-item.js
@@ -11,16 +11,12 @@ const DiscussionsItem = (props) => {
 		props.dispatch(getSingleDiscussion(props.id));
 	}
 
-	const imgSrc = `//${props.image.slice(11)}`;
-
-	console.log(imgSrc);
-
 	const dateStart = moment(props.dateStart).format('MMM DD, YYYY');
 	const dateEnd = moment(props.dateEnd).format('MMM DD, YYYY');
 
 	return (
 		<li>
-			<img src={imgSrc} alt="exhibition" className="ex-photo"/>
+			<img src={props.image} alt="exhibition" title="exhibition" className="ex-photo"/>
 			<div className="disc-item-container">
 				<h2><Link to={`/dashboard/discussion/${props.id}`} onClick={() => onTitleClick()}>{props.name}</Link></h2>
 		              <p>{props.venue.name} / {props.venue.address}</p>
